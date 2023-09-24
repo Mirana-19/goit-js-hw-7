2ac746402d9555b358fc0508a8ace91429e5a457
Marta Żak <martysik19@gmail.com>
2023-09-25 01:48:48 +0200

    added 1st gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click', showBackdrop);
+
+function renderGalleryMarkup(items) {
+  return (galleryEl.innerHTML = items
+    .map(
+      ({ original, preview, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join(''));
+}
+
+renderGalleryMarkup(galleryItems);
+
+function showBackdrop(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const backdrop = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" alt="${e.target.alt}">`
+  );
+
+  backdrop.show();
+
+  const closeBackdropByKey = e => {
+    if (e.code === 'Escape') {
+      backdrop.close();
+      window.removeEventListener('keydown', closeBackdropByKey);
+    }
+  };
+
+  window.addEventListener('keydown', closeBackdropByKey);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+// galleryEl.addEventListener('click', showBackdrop);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+   </li>`
+    )
+    .join('');
+}
